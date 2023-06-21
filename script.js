9fb3e105d84b1443df995b0b4c3964741afdf46c
MirandaMG20 <guadamiran@gmail.com>
2023-06-21 15:35:29 -0400

    Done!

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const addBtn = document.getElementById("add"); // console.log(addBtn);
 
-addBtn.addEventListener("click", () => addNewNote());
+const notes = JSON.parse(localStorage.getItem("notes"))
+
+if(notes) {
+    notes.forEach(note => addNewNote(note))
+}
+
+addBtn.addEventListener("click", () => addNewNote("Add a new note"));
 
 function addNewNote(text = "") { // console.log('clicked!')
     const note = document.createElement("div");
@@ -37,5 +43,34 @@ function addNewNote(text = "") { // console.log('clicked!')
         textArea.classList.toggle("hidden");
     })
 
+    textArea.addEventListener("input", (e) => {
+        const value = e.target.value;
+        // console.log(value);
+        main.innerHTML = value;
+
+        updateLS();
+    })
+
     document.body.appendChild(note);
 }
+
+function updateLS() {
+    const notesText = document.querySelectorAll("textarea");
+    console.log(notesText)
+
+    const notes = [];
+
+    notesText.forEach(note => notes.push(note.value))
+
+    // console.log(notes, notesText)
+
+    localStorage.setItem("notes", JSON.stringify(notes))
+}
+
+
+
+// localStorage.setItem("name", "Miranda");
+// localStorage.getItem("name");
+// localStorage.removeItem("name");
+// localStorage.setItem("studentNames", JSON.stringify())
+// JSON.parse(localStorage.getItem)
